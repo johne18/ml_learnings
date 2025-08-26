e126ad9ea63b85aa4d65f24f878ed01cd4c1b189
John Santiago <johne.santiago@gmail.com>
2025-08-25 20:37:03 -0400

    updating ngram analyses

--- SafeChat/common/clean_data.py
+++ SafeChat/common/clean_data.py
@@ -1,7 +1,7 @@
 import emoji
 
 import nltk
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, words
 from nltk.util import ngrams
 from nltk.tokenize import word_tokenize
 
@@ -10,19 +10,31 @@ def contains_emoji(text):
     return bool(emoji.emoji_count(text))
 
 
+def filter_non_standard_words(text):
+    nltk.download('words', quiet=True)
+
+    english_words = set(words.words())
+    tokens = word_tokenize(text)
+    filtered = [word for word in tokens if word.lower() in english_words]
+
+    return ' '.join(filtered)
+
+
 def generate_grams(text, n):
     nltk.download('punkt_tab', quiet=True)
-    # Tokenize the text
+
     tokens = word_tokenize(text)
-    # Create n-grams
     n_grams = list(ngrams(tokens, n))
+
     return n_grams
 
 
 def get_stop_words():
     nltk.download('stopwords', quiet=True)
+
     stop_words = set(stopwords.words('english'))
     stop_words.update(['I', 'You', 'YOU', 'like', 'The'])
+
     return stop_words
 
 
